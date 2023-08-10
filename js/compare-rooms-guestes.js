8bb3d71398104a67b2f7eae9eeec7603e2c5d18c
Vladislav Gernet <skeimorr@gmail.com>
2023-08-10 14:35:26 +0400

    room check module is added

---
+++ js/compare-rooms-guestes.js
@@ -0,0 +1,35 @@
+import {capacity, roomNumber} from './variables.js';
+import {numberOfGuests, numberOfRooms} from './constants.js';
+const {oneRoom, twoRooms, threeRooms, manyRooms} = numberOfRooms;
+const {oneGuest, twoGuests, threeGuests, noGuests} = numberOfGuests;
+
+const compareRoomsWithGuests = () => {
+  switch (roomNumber.value) {
+    case oneRoom:
+      capacity.value = oneGuest;
+      break;
+    case twoRooms:
+      if (capacity.value >= twoGuests) {
+        capacity.value = twoGuests;
+      } else if (capacity.value === '0') {
+        capacity.value = twoGuests;
+      }
+      break;
+    case threeRooms:
+      if (capacity.value >= threeGuests) {
+        capacity.value = threeGuests;
+      } else if (capacity.value === '0') {
+        capacity.value = threeGuests;
+      }
+      break;
+    case manyRooms: {
+      if (capacity.value > noGuests) {
+        capacity.value = noGuests;
+      }
+      break;
+    }
+  }
+};
+
+export {compareRoomsWithGuests};
+
