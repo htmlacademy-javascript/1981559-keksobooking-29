25b520c0f43ed4b90f0ecd2d6535de6b215f53af
Vladislav Gernet <skeimorr@gmail.com>
2023-08-13 16:05:22 +0400

    success message added

--- js/main.js
+++ js/main.js
@@ -8,12 +8,45 @@ import {pristine} from './validation.js';
 import {addPriceSlider} from './price-slider.js';
 import {resetForm} from './reset-form.js';
 import {blockSubmitButton, unblockSubmitButton} from './submit-button.js';
+import {isEscapeKey} from './util.js';
 
 deactivateForms();
 initMap();
 initUploadImage();
 initTimeCheck();
 addPriceSlider();
+
+const successTemplate = document.querySelector('#success')
+  .content
+  .querySelector('.success');
+const successContainer = successTemplate.querySelector('.success');
+
+const showSuccess = () => {
+  const successElement = successTemplate.cloneNode(true);
+  let returnToFormOnEscapeClick = () => {};
+
+  const removeSuccessMessage = () => {
+    document.removeEventListener('keydown', returnToFormOnEscapeClick);
+    successElement.remove();
+  };
+
+  returnToFormOnEscapeClick = (evt) => {
+    if (isEscapeKey(evt)) {
+      removeSuccessMessage();
+    }
+  };
+
+  document.addEventListener('keydown', returnToFormOnEscapeClick);
+  const onOutsideSuccessContainerClick = (evt) => {
+    const outsideErrorContainerClick = evt.composedPath().includes(successContainer) === false;
+    if (outsideErrorContainerClick) {
+      document.removeEventListener('click', onOutsideSuccessContainerClick);
+      removeSuccessMessage();
+    }
+  };
+  document.addEventListener('click', onOutsideSuccessContainerClick);
+  adForm.appendChild(successElement);
+};
 
 adForm.addEventListener('reset', resetForm);
 adForm.addEventListener('submit', (evt) => {
@@ -26,7 +59,7 @@ adForm.addEventListener('submit', (evt) => {
       .then(
         () => {
           console.log('ok in');
-          // showSuccess();
+          showSuccess();
         }
       )
       .then(
