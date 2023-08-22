7417b9e32d81749aaefd46dbf61f9be18ea260f7
Vladislav Gernet <skeimorr@gmail.com>
2023-08-22 19:16:20 +0400

    nice solution

--- js/map-filter-activation.js
+++ js/map-filter-activation.js
@@ -1,34 +1,26 @@
 import {mapForm} from './variables.js';
 import {createMarkers} from './create-map-markers.js';
 import {checkType, checkPrice, checkRooms, checkGuests, checkFeatures} from './map-filters.js';
+const arrayOfChecks = [checkType, checkPrice, checkRooms, checkGuests, checkFeatures];
 
 const activateMapFilter = (data, markers) => {
   mapForm.addEventListener('change', () => {
-    //     const compose = (...allFunctionsToCheck) => {
+    let filteredData = data;
+    arrayOfChecks.forEach((value) => {
+      filteredData = value(filteredData);
+    });
+    //изучить метод reduce
+    // const compose = (...allFunctionsToCheck) => {
     //       return (dataToFilter) => {
-    //        return allFunctionsToCheck.reduce((acc, fn) => {
-    //          console.log(acc)
-    //          console.log(fn)
-    //          return fn(acc);
+    //        return allFunctionsToCheck.reduce((previousFilteredData, filterData) => {
+    //          return filterData(previousFilteredData);
     //        }, dataToFilter);
     //       };
     //     };
     //
-    //     const filteredData = compose(
-    //       checkType,
-    //       checkPrice,
-    //       checkRooms,
-    //       checkGuests,
-    //       checkFeatures
-    //     )(data);
-    let filteredData = data;
-    const composedFunction = (someData) => checkFeatures(checkGuests(checkRooms(checkPrice(checkType(someData)))));
-    filteredData = composedFunction(filteredData);
-    filteredData = checkType(filteredData);
-    filteredData = checkPrice(filteredData);
-    filteredData = checkRooms(filteredData);
-    filteredData = checkGuests(filteredData);
-    filteredData = checkFeatures(filteredData);
+    //     const filteredData = compose(checkType, checkPrice, checkRooms, checkGuests, checkFeatures)(data);
+    // const composedFunction = (someData) => checkFeatures(checkGuests(checkRooms(checkPrice(checkType(someData)))));
+    // filteredData = composedFunction(filteredData);
     markers.clearLayers();
     createMarkers(filteredData, markers);
   });
